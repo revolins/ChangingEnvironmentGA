1e575c79dca1f770d9c1b085409b44ea4372a634
Jay Revolinsky <revolins@msu.edu>
2024-03-10 18:32:11 -0400

    format pd_beaker to df_, removed pngs and pdfs

--- test_mem.py
+++ test_mem.py
@@ -6,6 +6,6 @@ for i in range(1,6):
     subprocess.run(["python", "changing_environment_ga.py", "-c", f"config/pd_config_001.ini", "-o", f"pd_check/pd_static_test{i}_0.01_cost"])
     subprocess.run(["python", "changing_environment_ga.py", "-c", f"config/pd_config_05.ini", "-o", f"pd_check/pd_static_test{i}_0.5_cost"])
 
-subprocess.run(["python", "pd_beaker_three_all_bits_df.py"])
+subprocess.run(["python", "df_three_all_bits.py"])
 
-subprocess.run(["python", "pd_beaker_four_bits_of_memory_df.py"])
+subprocess.run(["python", "df_four_bits_of_memory.py"])
