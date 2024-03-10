0762d7a8f1c8186128f85ed4c4fb0aa0599d6760
ksuzue <suzuekar@msu.edu>
2024-03-10 01:28:05 -0500

    Commented out obsolute functions/portions of functions

--- main.py
+++ main.py
@@ -54,64 +54,64 @@ def get_mutated_population(population):
     return new_population
 
 # OBSOLETE
-def get_selected_population(population, environment):
-    """
-    Select a number of organisms based on tournament size, pick the best one, and append it to
-    a new population. Do this until the population is full.
-    """
-    new_population = []
-    for _ in range(NUMBER_OF_ORGANISMS):
-        orgs = [random.choice(population) for _ in range(TOURNAMENT_SIZE)]
-        new_population.append(get_best_organism(orgs, environment))
-    return new_population
+# def get_selected_population(population, environment):
+#     """
+#     Select a number of organisms based on tournament size, pick the best one, and append it to
+#     a new population. Do this until the population is full.
+#     """
+#     new_population = []
+#     for _ in range(NUMBER_OF_ORGANISMS):
+#         orgs = [random.choice(population) for _ in range(TOURNAMENT_SIZE)]
+#         new_population.append(get_best_organism(orgs, environment))
+#     return new_population
 
 # OBSOLETE
-def get_crowded_population(mutated_population, old_population, environment):
-    """
-    Crowding selection to preserve diversity and prevent premature convergence.
-    Compare each organism in the mutated population against another similar organism
-    from the old population and decide which of the two will survive. 
-    Returns a new population.
-    """
-    new_population = []
-    for org in mutated_population:
-        # Tournament-sized sample of random organisms from the old population
-        sample = [random.choice(old_population) for _ in range(TOURNAMENT_SIZE)] 
-        new_population.append(get_best_crowded_organism(org, sample, environment))
-    return new_population
+# def get_crowded_population(mutated_population, old_population, environment):
+#     """
+#     Crowding selection to preserve diversity and prevent premature convergence.
+#     Compare each organism in the mutated population against another similar organism
+#     from the old population and decide which of the two will survive. 
+#     Returns a new population.
+#     """
+#     new_population = []
+#     for org in mutated_population:
+#         # Tournament-sized sample of random organisms from the old population
+#         sample = [random.choice(old_population) for _ in range(TOURNAMENT_SIZE)] 
+#         new_population.append(get_best_crowded_organism(org, sample, environment))
+#     return new_population
 
 # OBSOLETE
-def get_best_organism(pop, environment):
-    """Gets the best org in a given population"""
-    best_org = pop[0]
-    for org in pop:
-        if org.is_better_than(best_org, environment):
-            best_org = org
-    return best_org
+# def get_best_organism(pop, environment):
+#     """Gets the best org in a given population"""
+#     best_org = pop[0]
+#     for org in pop:
+#         if org.is_better_than(best_org, environment):
+#             best_org = org
+#     return best_org
 
 # OBSOLETE
-def get_best_crowded_organism(new_org, sample, environment):
-    """Find the organism most similar to new_org in a given sample, return the better organism"""
-    most_similar = sample[0]
-    min_distance = float("inf")
-    for org in sample:
-        curr_dist = org.distance(new_org, environment)
-        if curr_dist < min_distance:
-            most_similar = org
-            min_distance = curr_dist
-    if new_org.is_better_than(most_similar, environment): 
-        return new_org
-    return most_similar
+# def get_best_crowded_organism(new_org, sample, environment):
+#     """Find the organism most similar to new_org in a given sample, return the better organism"""
+#     most_similar = sample[0]
+#     min_distance = float("inf")
+#     for org in sample:
+#         curr_dist = org.distance(new_org, environment)
+#         if curr_dist < min_distance:
+#             most_similar = org
+#             min_distance = curr_dist
+#     if new_org.is_better_than(most_similar, environment): 
+#         return new_org
+#     return most_similar
 
 # OBSOLETE
-def get_next_generation(population, environment):
-    """Get a mutated population, then get a selected population from it"""
-    new_population = get_mutated_population(population)
-    if CROWDING: # Crowding selection
-        new_new_population = get_crowded_population(new_population, population, environment)
-    else: # Tournament selection
-        new_new_population = get_selected_population(new_population, environment)
-    return new_new_population
+# def get_next_generation(population, environment):
+#     """Get a mutated population, then get a selected population from it"""
+#     new_population = get_mutated_population(population)
+#     if CROWDING: # Crowding selection
+#         new_new_population = get_crowded_population(new_population, population, environment)
+#     else: # Tournament selection
+#         new_new_population = get_selected_population(new_population, environment)
+#     return new_new_population
 
 def print_status(generation, population, environment):
     """Outputs information to console. Only used if VERBOSE is true"""
@@ -119,7 +119,7 @@ def print_status(generation, population, environment):
     print("Gen = {}  Pop = {}  Fit = {}".format(generation, population, average_fitness))
 
 def pd_evolve_population():
-    """Evolution loop for pd_org representation"""
+    """Evolution loop for PD org representation"""
     past_organisms = {}
     
     organisms = create_initial_population()
@@ -157,51 +157,51 @@ def pd_evolve_population():
     return output
 
 # OBSOLETE
-def evolve_population(reference_environment, alternative_environment):
-    """Evolve loop for other types of genome representation"""
-    #Set up the output lists
-    current_fitness_list = [("Generation", "Average_Fitness", 
-                    "Standard_Deviation")]
-    current_fitness_best = [("Generation", "Best_Fitness", "Best_Org")]
+# def evolve_population(reference_environment, alternative_environment):
+#     """Evolution loop for other types of genome representation"""
+#     #Set up the output lists
+#     current_fitness_list = [("Generation", "Average_Fitness", 
+#                     "Standard_Deviation")]
+#     current_fitness_best = [("Generation", "Best_Fitness", "Best_Org")]
 
-    reference_fitness_list = [("Generation", "Ref_Average_Fitness", 
-                    "Ref_Standard_Deviation")]
-    reference_fitness_best = [("Generation", "Ref_Best_Fitness", "Ref_Best_Org")]
+#     reference_fitness_list = [("Generation", "Ref_Average_Fitness", 
+#                     "Ref_Standard_Deviation")]
+#     reference_fitness_best = [("Generation", "Ref_Best_Fitness", "Ref_Best_Org")]
 
-    #Set up the other variables
-    current_environment = reference_environment
-    start_of_trimester_2 = int(floor(NUMBER_OF_GENERATIONS/3.0))
-    start_of_trimester_3 = int(floor(NUMBER_OF_GENERATIONS*2.0/3.0))
+#     #Set up the other variables
+#     current_environment = reference_environment
+#     start_of_trimester_2 = int(floor(NUMBER_OF_GENERATIONS/3.0))
+#     start_of_trimester_3 = int(floor(NUMBER_OF_GENERATIONS*2.0/3.0))
 
-    population = create_initial_population()
-    #In each generation...
-    for gen in range(NUMBER_OF_GENERATIONS):
-        #Check if the environment needs changed
-        if gen == start_of_trimester_2:
-            current_environment = alternative_environment
-        elif gen == start_of_trimester_3:
-            current_environment = reference_environment
-        #Get the next generation
-        population = get_next_generation(population, current_environment)
+#     population = create_initial_population()
+#     #In each generation...
+#     for gen in range(NUMBER_OF_GENERATIONS):
+#         #Check if the environment needs changed
+#         if gen == start_of_trimester_2:
+#             current_environment = alternative_environment
+#         elif gen == start_of_trimester_3:
+#             current_environment = reference_environment
+#         #Get the next generation
+#         population = get_next_generation(population, current_environment)
 
-        #Append the stats to the output lists
-        average_fitness, stdev, best_org, best_fitness = get_generation_stats(
-            population, current_environment)
-        current_fitness_list.append((gen, average_fitness, stdev))
-        current_fitness_best.append((gen, best_fitness, best_org))
+#         #Append the stats to the output lists
+#         average_fitness, stdev, best_org, best_fitness = get_generation_stats(
+#             population, current_environment)
+#         current_fitness_list.append((gen, average_fitness, stdev))
+#         current_fitness_best.append((gen, best_fitness, best_org))
         
-        average_fitness, stdev, best_org, best_fitness = get_generation_stats(
-            population, reference_environment)
-        reference_fitness_list.append((gen, average_fitness, stdev))
-        reference_fitness_best.append((gen, best_fitness, best_org))
+#         average_fitness, stdev, best_org, best_fitness = get_generation_stats(
+#             population, reference_environment)
+#         reference_fitness_list.append((gen, average_fitness, stdev))
+#         reference_fitness_best.append((gen, best_fitness, best_org))
 
-        if VERBOSE:
-            print_status(gen, population, current_environment)
+#         if VERBOSE:
+#             print_status(gen, population, current_environment)
     
-    #Return the output lists
-    result =  (current_fitness_list, current_fitness_best, 
-               reference_fitness_list, reference_fitness_best)
-    return result
+#     #Return the output lists
+#     result =  (current_fitness_list, current_fitness_best, 
+#                reference_fitness_list, reference_fitness_best)
+#     return result
 
 def get_generation_stats(population, environment):
     """Gets the stats for the given population"""
@@ -231,7 +231,7 @@ def set_global_variables(config):
     global START_TIME
     START_TIME = config.getfloat("DEFAULT", "start_time")
 
-    # Pre-specified config file
+    # Pre-specified in config file
     global VERBOSE
     VERBOSE = config.getboolean("DEFAULT", "verbose")
     global NUMBER_OF_ORGANISMS
@@ -245,44 +245,44 @@ def set_global_variables(config):
     global OUTPUT_FREQUENCY
     OUTPUT_FREQUENCY = config.getint("DEFAULT", "output_frequency")
 
-    if ORG_TYPE != "pd": # OBSOLETE
-        global TOURNAMENT_SIZE
-        TOURNAMENT_SIZE = config.getint("DEFAULT", "tournament_size")    
-    if ORG_TYPE == "string": # OBSOLETE
-        string_org.TARGET_STRING = config.get("DEFAULT", "target_string")
-        string_org.LETTERS = config.get("DEFAULT", "letters")
-    elif ORG_TYPE == "vector": # OBSOLETE
-        fitness_function_type_str = config.get("DEFAULT", "fitness_function_type")
-        global FITNESS_FUNCTION_TYPE
-        if fitness_function_type_str == "flat":
-            FITNESS_FUNCTION_TYPE = ff.flat_function
-        elif fitness_function_type_str == "sphere":
-            FITNESS_FUNCTION_TYPE = ff.sphere_function
-        elif fitness_function_type_str == "rosenbrock":
-            FITNESS_FUNCTION_TYPE = ff.rosenbrock_function
-        elif fitness_function_type_str == "rana":
-            FITNESS_FUNCTION_TYPE = ff.rana_function
-        elif fitness_function_type_str == "deceptive":
-            FITNESS_FUNCTION_TYPE = ff.deceptive
-        elif fitness_function_type_str == "schafferf7":
-            FITNESS_FUNCTION_TYPE = ff.schafferF7
-        else:
-            raise AssertionError("Unknown (but needed) function type (i.e. sphere)")
-        real_value_vector_org.LENGTH = config.getint("DEFAULT", "length")
-        range_minimum = config.getfloat("DEFAULT", "range_minimum")
-        real_value_vector_org.RANGE_MIN = range_minimum
-        range_maximum = config.getfloat("DEFAULT", "range_maximum")
-        real_value_vector_org.RANGE_MAX = range_maximum
-        mut_effect_size = config.getfloat("DEFAULT", "mutation_effect_size")
-        ff.MUTATION_EFFECT_SIZE = mut_effect_size
-        real_value_vector_org.MUTATION_EFFECT_SIZE = mut_effect_size
-        global ALTERNATE_ENVIRONMENT_CORR
-        ALTERNATE_ENVIRONMENT_CORR = config.getfloat(
-            "DEFAULT", "alternate_environment_corr")
-        global CROWDING
-        CROWDING = eval(config.get("DEFAULT", "crowding"))
+    # if ORG_TYPE != "pd": # OBSOLETE
+    #     global TOURNAMENT_SIZE
+    #     TOURNAMENT_SIZE = config.getint("DEFAULT", "tournament_size")    
+    # if ORG_TYPE == "string": # OBSOLETE
+    #     string_org.TARGET_STRING = config.get("DEFAULT", "target_string")
+    #     string_org.LETTERS = config.get("DEFAULT", "letters")
+    # elif ORG_TYPE == "vector": # OBSOLETE
+    #     fitness_function_type_str = config.get("DEFAULT", "fitness_function_type")
+    #     global FITNESS_FUNCTION_TYPE
+    #     if fitness_function_type_str == "flat":
+    #         FITNESS_FUNCTION_TYPE = ff.flat_function
+    #     elif fitness_function_type_str == "sphere":
+    #         FITNESS_FUNCTION_TYPE = ff.sphere_function
+    #     elif fitness_function_type_str == "rosenbrock":
+    #         FITNESS_FUNCTION_TYPE = ff.rosenbrock_function
+    #     elif fitness_function_type_str == "rana":
+    #         FITNESS_FUNCTION_TYPE = ff.rana_function
+    #     elif fitness_function_type_str == "deceptive":
+    #         FITNESS_FUNCTION_TYPE = ff.deceptive
+    #     elif fitness_function_type_str == "schafferf7":
+    #         FITNESS_FUNCTION_TYPE = ff.schafferF7
+    #     else:
+    #         raise AssertionError("Unknown (but needed) function type (i.e. sphere)")
+    #     real_value_vector_org.LENGTH = config.getint("DEFAULT", "length")
+    #     range_minimum = config.getfloat("DEFAULT", "range_minimum")
+    #     real_value_vector_org.RANGE_MIN = range_minimum
+    #     range_maximum = config.getfloat("DEFAULT", "range_maximum")
+    #     real_value_vector_org.RANGE_MAX = range_maximum
+    #     mut_effect_size = config.getfloat("DEFAULT", "mutation_effect_size")
+    #     ff.MUTATION_EFFECT_SIZE = mut_effect_size
+    #     real_value_vector_org.MUTATION_EFFECT_SIZE = mut_effect_size
+    #     global ALTERNATE_ENVIRONMENT_CORR
+    #     ALTERNATE_ENVIRONMENT_CORR = config.getfloat(
+    #         "DEFAULT", "alternate_environment_corr")
+    #     global CROWDING
+    #     CROWDING = eval(config.get("DEFAULT", "crowding"))
 
-    elif ORG_TYPE == "pd":
+    if ORG_TYPE == "pd":
         global SELECTION_BY_STATIC_COMPETITOR
         SELECTION_BY_STATIC_COMPETITOR = config.getboolean("DEFAULT", "selection_by_static_competitor")
         pd_tournament.NUMBER_OF_ROUNDS = config.getint("DEFAULT", "number_of_rounds")
@@ -322,17 +322,17 @@ def generate_data():
     config_dest = os.path.join(OUTPUT_FOLDER, config_filename)
     shutil.copyfile(CONFIG_FILE, config_dest)
 
-    if ORG_TYPE == "vector": # OBSOLETE
-        fitness_function = ff.Fitness_Function(FITNESS_FUNCTION_TYPE, real_value_vector_org.LENGTH)
-        fitness_function.create_fitness2(ALTERNATE_ENVIRONMENT_CORR)
-        reference_environment = fitness_function.fitness1_fitness
-        alternative_environment = fitness_function.fitness2_fitness
-    elif ORG_TYPE == "string": # OBSOLETE
-        reference_environment = string_org.default_environment
-        alternative_environment = string_org.hash_environment
-    if ORG_TYPE != "pd": # OBSOLETE
-        experienced_fits, experienced_bests, reference_fits, reference_bests = evolve_population(
-        reference_environment, alternative_environment)
+    # if ORG_TYPE == "vector": # OBSOLETE
+    #     fitness_function = ff.Fitness_Function(FITNESS_FUNCTION_TYPE, real_value_vector_org.LENGTH)
+    #     fitness_function.create_fitness2(ALTERNATE_ENVIRONMENT_CORR)
+    #     reference_environment = fitness_function.fitness1_fitness
+    #     alternative_environment = fitness_function.fitness2_fitness
+    # elif ORG_TYPE == "string": # OBSOLETE
+    #     reference_environment = string_org.default_environment
+    #     alternative_environment = string_org.hash_environment
+    # if ORG_TYPE != "pd": # OBSOLETE
+    #     experienced_fits, experienced_bests, reference_fits, reference_bests = evolve_population(
+    #     reference_environment, alternative_environment)
 
     if ORG_TYPE == "pd":
         output = pd_evolve_population()
@@ -354,6 +354,7 @@ def generate_data():
     #     save_table_to_file(reference_bests, reference_best_filename)
     #     if ORG_TYPE == "vector":
     #         save_string_to_file(str(fitness_function.correlation()), corr_filename)
+        
     time_filename = join_path("time.dat")     
     start_time = datetime.datetime.fromtimestamp(START_TIME)
     end_time = datetime.datetime.now()
