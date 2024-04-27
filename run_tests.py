c00ef4203ffe330d113874fbe7f37d860a497c2e
Jay Revolinsky <revolins@msu.edu>
2024-04-26 20:54:07 -0400

    Update tests file

--- run_tests.py
+++ run_tests.py
@@ -1,32 +1,13 @@
 import subprocess
 
 # Hypothesis 1 - Original Experiment Re-Creation and Correlation Tests
-subprocess.run(['python', 'test.py', '--mut_rat', '0.01'])
-subprocess.run(['python', 'test.py', '--mut_rat', '0.1'])
-subprocess.run(['python', 'test.py', '--static'])
+# subprocess.run(['python', 'test.py', '--mut_rat', '0.01'])
+# subprocess.run(['python', 'test.py', '--mut_rat', '0.1'])
+# subprocess.run(['python', 'test.py', '--static'])
 
 subprocess.run(['python', 'test.py', '--mut_rat', '0.01', '--hybrid'])
-subprocess.run(['python', 'test.py', '--mut_rat', '0.1', '--hybrid'])
-subprocess.run(['python', 'test.py', '--static', '--hybrid'])
-
-# Hypothesis 2 - Static and No Mutation
-subprocess.run(['python', 'test.py', '--static', '--hybrid', '--remove_mem_limit', '--noise', '0.01'])
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--noise', '0.01'])
-
-subprocess.run(['python', 'test.py', '--static', '--hybrid', '--remove_mem_limit', '--noise', '0.05'])
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--noise', '0.05'])
-
-subprocess.run(['python', 'test.py', '--static', '--hybrid', '--remove_mem_limit', '--noise', '0.1'])
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--noise', '0.1'])
-
-subprocess.run(['python', 'test.py', '--static', '--hybrid', '--remove_mem_limit', '--noise', '0.2'])
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--noise', '0.2'])
-
-subprocess.run(['python', 'test.py', '--static', '--hybrid', '--remove_mem_limit', '--noise', '0.5'])
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--noise', '0.5'])
-
-subprocess.run(['python', 'test.py', '--static', '--hybrid', '--remove_mem_limit', '--noise', '1.0'])
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--noise', '1.0'])
+# subprocess.run(['python', 'test.py', '--mut_rat', '0.1', '--hybrid'])
+# subprocess.run(['python', 'test.py', '--static', '--hybrid'])
 
 ##### Hypothesis 2 - Low Mutation
 subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--mut_rat', '0.01', '--noise', '0.01'])
@@ -40,18 +21,6 @@ subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--mut_ra
 subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--mut_rat', '0.01', '--noise', '0.5'])
 
 subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--mut_rat', '0.01', '--noise', '1.0'])
-##### Hypothesis 2 - High Mutation
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--mut_rat', '0.1', '--noise', '0.01'])
-
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--mut_rat', '0.1', '--noise', '0.05'])
-
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--mut_rat', '0.1', '--noise', '0.1'])
-
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--mut_rat', '0.1', '--noise', '0.2'])
-
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--mut_rat', '0.1', '--noise', '0.5'])
-
-subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--mut_rat', '0.1', '--noise', '1.0'])
 
 ##### Hypothesis 3 - Friendly
 subprocess.run(['python', 'test.py', '--hybrid', "--remove_mem_limit", '--mut_rat', '0.01', '--noise', '0.01', '--env_seed', 'coop'])
